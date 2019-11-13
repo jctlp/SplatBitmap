6e8b077623a47047c85e685e9f66db386db79666
James Connell <jconnell@tlpsoftware.com>
2019-11-13 10:11:28 -0500

    Initial commit

---
+++ SplatBug/SplatBug/SplatBug.Android/Foo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Splat;
+
+namespace SplatBug.Droid
+{
+    public class Foo
+    {
+        public void Bar(IBitmap source, int radius)
+        {
+            Bitmap native = source.ToNative();
+            IBitmap bitmap = native.FromNative();
+        }
+    }
+}
